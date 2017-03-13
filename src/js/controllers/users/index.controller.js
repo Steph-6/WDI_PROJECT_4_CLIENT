ab07e4882c4ec7616adecef9bc2213e668861aca
Steph-6 <Steph.robinson95@gmail.com>
2017-03-13 12:58:17 +0000

    styled login and register

--- src/js/controllers/users/index.controller.js
+++ src/js/controllers/users/index.controller.js
@@ -5,14 +5,5 @@ angular
 UsersIndexCtrl.$inject = ['User', 'CurrentUserService'];
 function UsersIndexCtrl(User, CurrentUserService){
   const vm = this;
-
-  // User
-  //   .get({id: CurrentUserService.currentUser.id})
-  //   .$promise
-  //   .then(data => {
-  //     vm.user = data;
-  //     console.log(vm.user, 'current user');
-  //   });
-
   vm.users = User.query();
 }
